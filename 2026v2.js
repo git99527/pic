fdd0949162ab1cabf592f0d419edd5146c9ddee0
git99527 <45354442+git99527@users.noreply.github.com>
2026-02-07 22:02:22 +0800

    Add shipping notice for Lunar New Year

---
+++ 2026v2.js
@@ -0,0 +1,3 @@
+document.write("<p style='text-align:center;font-size:25px;color:#D9534F;margin-bottom: 6px;'>春節暫停出貨通知</p>");
+document.write("<p style='font-size:18px;text-align:center;color:#5BC0DE;margin-bottom: 6px;'><span><b>2月8日18:00</b></span><span>前(含當日)訂單將正常出貨，</span><span><b>2月9日-2月23日</b></span><span> 暫停出貨，期間官網仍可正常下單并統一於</span><span><b style='color:#F0AD4E;'>2月24日起將訂單依序出貨。</b></span></p>");
+document.write("<p style='font-size:18px;text-align:center;color:#5BC0DE;margin-bottom: 6px;'>※ 若需要趕在年節前收到商品，請選擇在2月8日前訂購</p>");
